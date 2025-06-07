29d35fc10dea84fe2a9464d930d60ecbcf5f581b
Niel <dyprih@gmail.com>
2025-06-07 12:46:33 +0200

    added ft_upper and ft_lower functions #3

--- libft/sources/ft_toupper.c
+++ libft/sources/ft_toupper.c
@@ -1,28 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: niel <niel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/06 01:32:41 by niel              #+#    #+#             */
-/*   Updated: 2025/06/07 12:14:20 by niel             ###   ########.fr       */
+/*   Created: 2025/06/07 12:05:26 by niel              #+#    #+#             */
+/*   Updated: 2025/06/07 12:13:43 by niel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(void *dest, const void *src, int n)
+int	ft_toupper(int c)
 {
-	unsigned char		*ptr;
-	const unsigned char	*s;
-	int					i;
-
-	i = 0;
-	ptr = (unsigned char *)dest;
-	s = (const unsigned char *)src;
-	while (i < n)
-	{
-		ptr[i] = s[i];
-		i++;
-	}
-	return (dest);
+	if (c >= 97 && c <= 122)
+		return (c - 32);
+	else
+		return (c);
 }
