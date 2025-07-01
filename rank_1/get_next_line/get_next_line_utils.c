8c9487ab71b2aeec13dc6526f189f38fa511a045
user <user@MBP-user.wifi.vscht.cz>
2025-07-01 10:31:39 +0200

    get_next_line_utills added

--- rank_1/get_next_line/get_next_line_utils.c
+++ rank_1/get_next_line/get_next_line_utils.c
@@ -3,11 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 12:50:55 by dprikhod          #+#    #+#             */
-/*   Updated: 2025/06/24 12:56:20 by dprikhod         ###   ########.fr       */
+/*   Updated: 2025/07/01 10:14:55 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*ptr;
+	size_t	size;
+
+	size = ft_strlen(s) + 1;
+	ptr = malloc(size);
+	if (!ptr)
+		return (NULL);
+	ft_strlcpy(ptr, s, size);
+	return (ptr);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*res;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	res = malloc(len1 + len2 + 1);
+	if (!res)
+		return (NULL);
+	ft_memcpy(res, s1, len1);
+	ft_memcpy(res + len1, s2, len2);
+	res[len1 + len2] = '\0';
+	return (res);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	sub = malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	ft_memcpy(sub, s + start, len);
+	sub[len] = '\0';
+	return (sub);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	n;
+
+	n = 0;
+	while (*str != '\0')
+	{
+		n++;
+		str++;
+	}
+	return (n);
+}
