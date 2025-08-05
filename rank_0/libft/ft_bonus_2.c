27d3076a4522d29cb4ea614562a7e5546241edee
dprikhod <dprikhod@student.42prague.com>
2025-08-05 16:36:36 +0200

    ft_lstsize

--- rank_0/libft/ft_bonus_2.c
+++ rank_0/libft/ft_bonus_2.c
@@ -10,9 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
+/*
+## Parameters
+lst: The beginning of the list.
+## Return value
+The length of the list.
+*/
 int	ft_lstsize(t_list *lst)
 {
-	
+	int	size;
+
+	if (!lst)
+		return (0);
+	size = 1;
+	while ((lst->next))
+	{
+		size++;
+		lst = lst->next;
+	}
+	return (size);
 }
+
+/*
+Iterates through the list ’lst’ and applies the
+function ’f’ to the content of each node.
+## Parameters
+lst: The address of a pointer to a node.
+f: The address of the function to apply to each
+node’s content.
+## Return value
+None
+*/
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+}
+
