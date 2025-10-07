2a8ea444e699a14bdeb0e42949fc628a74cf7e0c
Niel <dyprih@gmail.com>
2025-10-07 16:01:54 +0200

    pipex start

---
+++ rank_2/pipex/pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 06:01:59 by dprikhod          #+#    #+#             */
+/*   Updated: 2025/10/06 13:50:52 by dprikhod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
