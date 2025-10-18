549847ec03776f94a529a11df5c27dcde4b40ba3
Niel <dyprih@gmail.com>
2025-10-18 14:49:13 +0200

    main.c

--- rank_2/pipex/main.c
+++ rank_2/pipex/main.c
@@ -6,19 +6,19 @@
 /*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 06:00:34 by dprikhod          #+#    #+#             */
-/*   Updated: 2025/10/06 13:49:47 by dprikhod         ###   ########.fr       */
+/*   Updated: 2025/10/07 21:51:18 by dprikhod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int ac, char **argv, char **env)
+int	main(int argc, char **argv, char **env)
 {
 	if (ac == 4)
 	{
+		pipex(
 	}
 	else
-	{
 		ft_printf("invalid arguments\n");
-	}
+	fork()
 }
