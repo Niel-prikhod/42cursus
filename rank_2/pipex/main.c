5fbaafa97e773a3fba6ffc8806b8b16d7df70b37
dprikhod <dprikhod@student.42prague.com>
2025-10-21 18:28:47 +0200

    parser started

--- rank_2/pipex/main.c
+++ rank_2/pipex/main.c
@@ -6,19 +6,32 @@
 /*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 06:00:34 by dprikhod          #+#    #+#             */
-/*   Updated: 2025/10/07 21:51:18 by dprikhod         ###   ########.fr       */
+/*   Updated: 2025/10/21 18:27:59 by dprikhod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	pi_parse(int argc, char **argv, t_pipex *data)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+	}
+}
+
 int	main(int argc, char **argv, char **env)
 {
-	if (ac == 4)
-	{
-		pipex(
-	}
-	else
-		ft_printf("invalid arguments\n");
-	fork()
+	t_pipex	*data;
+
+	if (argc < 5)
+		return (INVALID_ARGS);
+	data->infile = open(argv[0], O_RDONLY);
+	data->outfile = open(argv[argc - 1], O_TRUNC | O_WRONLY | O_CREAT);
+	if (!data->infile || !data->outfile)
+		return (INVALID_FILE);
+	pi_parse(argc, argv, data);
+	return (0);
 }
--- rank_2/pipex/pipex.h
+++ rank_2/pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 06:01:59 by dprikhod          #+#    #+#             */
-/*   Updated: 2025/10/06 13:50:52 by dprikhod         ###   ########.fr       */
+/*   Updated: 2025/10/21 17:39:53 by dprikhod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,19 @@
 # include <stdlib.h>
 # include <sys/wait.h>
 # include <unistd.h>
+# include <fcntl.h>
+
+typedef enum e_error_code
+{
+	INVALID_FILE = 2,
+	INVALID_ARGS = 3,
+}	t_error_code;
+
+typedef struct s_pipex
+{
+	char	**cmd;
+	int		infile;
+	int		outfile;
+}	t_pipex;
 
 #endif
