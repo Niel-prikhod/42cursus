15fa15eb999978917328e878fb43d22a80da3263
Niel <dyprih@gmail.com>
2026-01-14 14:18:30 +0100

    resolve issue with incorrect fd assignment

--- rank_2/pipex/pipex_execution.c
+++ rank_2/pipex/pipex_execution.c
@@ -6,7 +6,7 @@
 /*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 10:58:30 by dprikhod          #+#    #+#             */
-/*   Updated: 2025/11/11 11:54:02 by dprikhod         ###   ########.fr       */
+/*   Updated: 2026/01/14 14:07:54 by dprikhod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,9 @@ bool	ft_exec_cmd(int *fd, char **path, char **argv, char **envp)
 		return (perror("CHANGING_OUTPUT_ERROR"), false);
 	close(fd[1]);
 	new_path = ft_get_path(path, argv[0]);
-	if (execve(new_path, argv, envp))
+	if (new_path == NULL)
+		return (perror("COMMAND_NOT_FOUND"), false);
+	if (execve(new_path, argv, envp) == -1)
 		return (perror("EXEC_ERROR"), false);
 	return (true);
 }
@@ -58,18 +60,20 @@ bool	ft_pipes_handler(t_pipex *data, char **envp)
 	if (pid1 < 0)
 		return (perror("FORK_ERROR"), false);
 	cmd1_pipe[0] = data->infile;
-	cmd1_pipe[1] = fd[0];
+	cmd1_pipe[1] = fd[1];
 	cmd2_pipe[1] = data->outfile;
-	cmd2_pipe[0] = fd[1];
+	cmd2_pipe[0] = fd[0];
 	if (pid1 == 0)
 	{
-		close(fd[1]);
+		close(fd[0]);
 		ft_exec_cmd(cmd1_pipe, data->path, data->cmd->content, envp);
 	}
 	pid2 = fork();
+	if (pid2 < 0)
+		return (perror("FORK_ERROR"), false);
 	if (pid2 == 0)
 	{
-		close(fd[0]);
+		close(fd[1]);
 		ft_exec_cmd(cmd2_pipe, data->path, data->cmd->next->content, envp);
 	}
 	close(fd[1]);
